6462d4637647f8c47a4c45c217c1d9e0a04d142e
Messias-Acacy <130923301+Messias-Acacy@users.noreply.github.com>
2023-07-09 23:27:13 -0300

    Add files via upload

--- partida.py
+++ partida.py
@@ -10,39 +10,32 @@ Pecadojogador1 = ["\033[0;31;1mT\033[m","\033[0;31;1mC\033[m","\033[0;31;1mR\033
 
 while True:
     print(tabuleiro_jogo())
+    
     if jogador == True:
-        print("Jogador 1")
-        posicao_inicial = input("digite a posição para selecionar  a aa peça: ")
-        posicao_final= input('Digite a posicao para mover a peça: ')
-        cash = posicao_inicial
-        cash = PosicaoInicial(cash)
+         print("Jogador atual: Jogador 1")
+    else:
+        print("Jogador atual: Jogador 2")
+
+    posicao_inicial = input("digite a posição para selecionar  a aa peça: ")
+    posicao_final= input('Digite a posicao para mover a peça: ')
+    cash = posicao_inicial
+    cash = PosicaoInicial(cash)
+    
+    if jogador == True:
         for x in range (len(Pecadojogador)):
             if tabuleiro[cash[0]][cash[1]] == Pecadojogador[x]:
                 MoverPeca(posicao_inicial,posicao_final)
                 jogador = False
                 break
         else:
-
-            if tabuleiro2[cash[0]][cash[1]] == '-':
-                print("Você deve escolher uma peça para jogar!")
-            else:
                 print("Você só pode jogar com suas peças!")
     else:
-        print("Jogador 2")
-        posicao_inicial = input("digite a posição para selecionar  a ab peça: ")
-        posicao_final= input('Digite a posicao para mover a peça: ')
-        cash = posicao_inicial
-        cash = PosicaoInicial(cash)
         for x in  range(len(Pecadojogador1)):
             if tabuleiro[cash[0]][cash[1]] == Pecadojogador1[x]:
                 MoverPeca(posicao_inicial,posicao_final)
                 jogador = True
                 break
         else:
-
-            if tabuleiro2[cash[0]][cash[1]] == '-':
-                print("Você deve escolher uma peça para jogar!")
-            else:
                 print("Você só pode jogar com suas peças!")
         
 
